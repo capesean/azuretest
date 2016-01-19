6e09cef0836f3e137e4c7cb3978a9b09699ed209
Sean Walsh <seanmatthewwalsh@gmail.com>
2016-01-19 14:03:44 +0200

    adding files

---
+++ src/azuretest/Startup.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IXESHA
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public static void Main(string[] args)
+        {
+            var hostingConfiguration = WebHostConfiguration.GetDefault(args);
+            var application = new WebHostBuilder()
+                .UseConfiguration(hostingConfiguration)
+                .UseIISPlatformHandlerUrl()
+                .UseStartup<Startup>()
+                .Build();
+
+            application.Run();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseIISPlatformHandler();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "DefaultApi",
+                    template: "api/{controller}");
+            });
+
+        }
+    }
+}
---
+++ src/azuretest/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+
+namespace IXESHA.API
+{
+    [Route("/api/[controller]")]
+    public class TestController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok(
+                new { Time = DateTime.Now }
+               );
+        }
+    }
+}
